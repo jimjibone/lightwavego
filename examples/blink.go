bdf5a7b3491ffba90b1135d6017d9ebe919f5925
jimjibone <jamesreuss@gmail.com>
2015-01-18 15:55:51 +0000

    updated blink example to make sure of newer lwgo

--- examples/blink.go
+++ examples/blink.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
     fmt.Println("lwgo blink!")
+    defer lwgo.Shutdown()
 
     // Set up the LightwaveRF TX instance.
     lwtx := lwgo.NewLwTx()
@@ -18,14 +19,14 @@ func main() {
     lightOff, _ := lwgo.NewMessage([]byte{0x4,0x0,0x3,0x0,0x5,0x9,0x3,0x0,0x1,0x2}, 0, 0) // off
 
     // Send a couple of these messages.
-    for i := 0; i < 5; i++ {
+    for i := 0; i < 2; i++ {
         fmt.Println("lightOn:", lightOn)
         lwtx.Send(lightOn)
-        time.Sleep(2 * time.Second)
+        time.Sleep(4 * time.Second)
 
         fmt.Println("lightOff:", lightOff)
         lwtx.Send(lightOff)
-        time.Sleep(2 * time.Second)
+        time.Sleep(4 * time.Second)
     }
 
     fmt.Println("all done")
